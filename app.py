d392c045d017757fdcaf83152a4458199f8a4bc3
Shuraim Shafiulla <92625656+Shuraimi@users.noreply.github.com>
2025-06-25 11:12:26 +0530

    Update app.py
    changed last part of code

--- app.py
+++ app.py
@@ -84,5 +84,8 @@ def translate_video():
             if os.path.exists(f):
                 os.remove(f)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # fallback for local
+    app.run(host='0.0.0.0', port=port)
